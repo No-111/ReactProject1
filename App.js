b9b22ee122a4c9e33d8c69baf71e83eb30f7f9e0
Kawin Chuangyanyong <90861008+No-111@users.noreply.github.com>
2022-07-29 21:34:55 +0700

    commit

--- App.js
+++ App.js
@@ -1,18 +1,19 @@
 import { StyleSheet, Text, View, Button } from 'react-native'
 import React from 'react'
-import User from './components/User'
-import Lotsofgreeting from './components/lotsofgreeting'
-import MycustomTextWith from './components/MycustomTextWith'
-import Count from './components/Count'
-import Inputtext from './components/Inputtext'
-import UsernameandPassword from './components/UsernameandPassword'
-import AlertEx from './components/AlertEx'
-import ImagewithText from './components/ImagewithText'
-import ButtonEx from './components/ButtonEx'
-import TouchableEx from './components/TouchableEx'
-import TouchablePr from './components/TouchablePr'
-import Dynamicstyle from './components/Dynamicstyle'
-import ModelExample from './components/ModelExample'
+// import User from './components/User'
+// import Lotsofgreeting from './components/lotsofgreeting'
+// import MycustomTextWith from './components/MycustomTextWith'
+// import Count from './components/Count'
+// import Inputtext from './components/Inputtext'
+// import UsernameandPassword from './components/UsernameandPassword'
+// import AlertEx from './components/AlertEx'
+// import ImagewithText from './components/ImagewithText'
+// import ButtonEx from './components/ButtonEx'
+// import TouchableEx from './components/TouchableEx'
+// import TouchablePr from './components/TouchablePr'
+// import Dynamicstyle from './components/Dynamicstyle'
+// import ModelExample from './components/ModelExample'
+import ModelPC from './components/ModelPC'
 
 export default function App() {
 
@@ -33,7 +34,8 @@ export default function App() {
       {/* <TouchableEx/> */}
       {/* <TouchablePr/> */}
       {/* <Dynamicstyle/> */}
-      <ModelExample/>
+      {/* <ModelExample/> */}
+      <ModelPC/>
     </View>
     
   )
